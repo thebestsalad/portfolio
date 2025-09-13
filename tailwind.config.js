945a1a7145d0436564095f2a9874a9b462696f75
thebestsalad <cesar.diab@my.utsa.edu>
2025-09-13 02:21:19 -0500

    feat: initial portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkmode: 'class',
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
